d8ed6478cfe13c77b1db33c592647819cd7fad31
Esubalew Workineh <esubwork@gmail.com>
2017-05-24 16:34:14 +0300

    Prime Count

---
+++ primeCount/src/com/esubalew/Main.java
@@ -0,0 +1,22 @@
+package com.esubalew;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       int result = primeCount(10,30);
+        System.out.println(result);
+
+    }
+
+    public static int primeCount(int start, int end){
+        int count =0;
+        int number;
+        for(int i = start; i <= end; i++){
+            boolean isPrime= true;
+            for()
+        }
+        return count;
+    }
+
+}
